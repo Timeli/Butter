5bd1a22841d9bc3d2b3590eac23b5efe42cdcd9f
Timely <timbelentsov@gmail.com>
2021-12-18 18:52:09 +0300

    full bridge

--- Butter Project/Assets/Scripts/Button/Button.cs
+++ Butter Project/Assets/Scripts/Button/Button.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    [SerializeField] private Transform _bridge;
+    [SerializeField] private Transform _startPointToBridge;
+    [SerializeField] private Transform _endPointToBridge;
+
+    private Vector3 _deltaPosition;
+    private int _bridgeSpeed = 120;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);   
+        Vector3 length = _startPointToBridge.position - _endPointToBridge.position;
+        _deltaPosition = length / _bridgeSpeed;
+        InitialCreateBridge();
     }
 
-    private void InitialBridge(float _duration)
+
+    private void InitialCreateBridge()
     {
-
+        StartCoroutine(CreateBridge());
     }
 
-    private IEnumerator MoveBridge()
+
+    private IEnumerator CreateBridge()
+    {
+        for (int i = 0; i < _bridgeSpeed; i++)
+        {
+            _startPointToBridge.position -= _deltaPosition;
+            yield return null;
+        }
+    }
 }
